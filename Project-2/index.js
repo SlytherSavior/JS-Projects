cd591b2d5e21d688b4cf2c58c67c8ec1d970e1ff
Slyther Savior <shrijanpoudel9@gmail.com>
2024-06-20 16:43:14 +0545

    basic finalize styling , js now

---
+++ Project-2/index.js
@@ -0,0 +1,8 @@
+const pause = document.getElementById('pause') ; 
+const play = document.getElementById('play') ; 
+const reset = document.getElementById('reset'); 
+
+
+reset.addEventListener('click', function(){
+  alert("hello world");
+}) 
